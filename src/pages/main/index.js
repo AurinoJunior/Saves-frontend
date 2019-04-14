d1a55aab97cf07c99ba562f7270afa85330dbf7c
Aurino Junior <aurino_aj@hotmail.com>
2019-04-14 17:46:15 -0300

    Service mandando request para criar box

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
 
+import api from '../../services/boxes-api';
 import logo from '../../assets/logo-original.png'
 import './style.css'
 
 export default class main extends Component {
+  state = {
+    newBox: ''
+  }
+
+  //A funcao e criada no estilo arrow function pelo fato de não ter this então o this sera associado a classe e não a funcao.
+  handleSubmit = async event => {
+    event.preventDefault();
+
+    const response = await api.post('/boxes',{
+      title: this.state.newBox
+    })
+
+    console.log(response.data)
+  }
+
+  handleInput = event => {
+    this.setState({newBox: event.target.value});
+  }
   render() {
     return (
       <div id="main-container">
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <img src={logo}/>
-          <input placeholder="Digite o nome da Box"/>
+          <input
+            onChange={this.handleInput}
+            placeholder="Digite o nome da Box"
+          />
           <button type="submit">Criar</button>
         </form>
       </div>
--- src/services/boxes-api.js
+++ src/services/boxes-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL = 'https://saves-backend.herokuapp.com'
+  baseURL:'https://saves-backend.herokuapp.com',
 })
 
 export default api

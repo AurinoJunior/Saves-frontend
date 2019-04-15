0a67989952c1aa0d1d6d98fdda1a8d7990700e03
Aurino Junior <aurino_aj@hotmail.com>
2019-04-14 21:49:36 -0300

    Adicionando real-time na aplicacao

--- src/pages/box/index.js
+++ src/pages/box/index.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { FaFile } from 'react-icons/fa';
 import {distanceInWords} from 'date-fns'
-import {pt} from 'date-fns/locale/pt'
+import Dropzone from 'react-dropzone'
+import socket from 'socket.io-client'
 
 import api from '../../services/boxes-api';
 
@@ -15,10 +16,32 @@ export default class box extends Component {
   }
 
   async componentDidMount(){
+    this.subiscribeToNewFiles();
     const box = this.props.match.params.id
     const response = await api.get(`/boxes/${box}`)
 
     this.setState({box:response.data})
+  }
+
+  subiscribeToNewFiles = () => {
+    const box = this.props.match.params.id
+    const io = socket('https://saves-backend.herokuapp.com')
+
+    io.emit('connectRoom', box)
+
+    io.on('file', data => {
+      this.setState({ box: { ...this.state.box, files: [data, ...this.state.box.files] } })
+    })
+  }
+
+  handleUpload = files =>{
+    files.forEach(file =>{
+      const data = new FormData(); 
+
+      data.append('file', file);
+
+      api.post(`/boxes/${this.state.box._id}/files`, data);
+    })
   }
 
   render() {
@@ -28,19 +51,29 @@ export default class box extends Component {
           <img src={logo} />
           <h1>{this.state.box.title}</h1>
         </header>
+
+        <Dropzone onDropAccepted={this.handleUpload}>
+          {({getRootProps, getInputProps}) => (
+            <div className="upload" {...getRootProps()}>  
+              <input {...getInputProps()}/>
+
+              <p>Arraste arquivos ou clique aqui</p>
+            </div>
+          )}
+        </Dropzone>
+
         <ul>
           {/* Validação para ver se o array files não está vazio */}
           {this.state.box.files && this.state.box.files.map( file => (
-            <li>
+            <li key={file._id}>
             <a className="fileInfo" target="blank" href={file.url}>
               <FaFile size={24} color="#A5Cfff"/>
               <strong>{file.title}</strong>
             </a>
-              <span> há {" "}
+              <span>
               {
-                distanceInWords(file.createdAt, new Date(), 
-                {locale: pt})
-              } {" "} atrás
+                distanceInWords(file.createdAt, new Date())
+              }
               </span>
             </li>
           ))}
